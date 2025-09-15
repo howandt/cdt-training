fa46ae42c6600ec421e46e24183c67ba28a9fd69
howandt <howandt@gmail.com>
2025-09-15 13:10:14 +0200

    � Tilføjet @ alias til Next.js Webpack config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+const path = require('path');
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    return config;
+  }
 };
 
-module.exports = nextConfig;
 
